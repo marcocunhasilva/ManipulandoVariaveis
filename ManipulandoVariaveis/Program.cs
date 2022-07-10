bf8818d3490826acb6e565db732f130e07d46bba
marcocunhasilva <marcocunhasilvadeveloper@gmail.com>
2022-07-09 23:06:27 -0300

    Add project files.

---
+++ ManipulandoVariaveis/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManipulandoVariaveis
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Manipulando Variáveis");
+            Console.WriteLine("=====================");
+            Console.WriteLine(""); 
+            Console.WriteLine("");
+            Console.WriteLine("int i;");
+            int i;
+            Console.ReadKey();
+            Console.WriteLine("");
+            Console.WriteLine("i = 10;");
+            Console.ReadKey();
+            Console.WriteLine("");
+            int i = 10;
+            Console.WriteLine("i2 = i = 10;");
+            i2 = i = 10;            
+            Console.ReadKey();
+            Console.WriteLine("");
+        }
+    }
+}
